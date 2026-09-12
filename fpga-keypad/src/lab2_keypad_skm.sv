c4b277cf67f370110f95835ee2dd31d51d19097e
Stephen <stephen@kantimahanty.com>
2026-09-11 23:51:09 -0700

    Excellence specs

--- fpga-keypad/src/lab2_keypad_skm.sv
+++ fpga-keypad/src/lab2_keypad_skm.sv
@@ -3,13 +3,14 @@
 // Top-level module that connects two seven-segment displays
 // and multiplexes them to save on resources.
 
-module lab2_skm #(parameter width = 24, parameter [width-1:0] max_count = 2999999) (
+module lab2_skm (
     input  logic [3:0] cols, // Column inputs from the keypad
     output logic [3:0] rows, // Row outputs to the keypad
     output logic [3:0] led // LED outputs from the FPGA
 );
 
     logic int_osc;
+    logic [2:0] scan_out;
 
     // Internal high-speed oscillator
     HSOSC #(.CLKHF_DIV(2'b01))
@@ -18,13 +19,26 @@ module lab2_skm #(parameter width = 24, parameter [width-1:0] max_count = 299999
     // Sets up a counter for the scanner
     lab2_scanner #(
         .width     (24),
-        .max_count (2_999_999)
+        .max_count (1_499_999)
     ) scan (
-        .reset  (1'b1),
+        .reset_n  (1'b1),
         .clk    (int_osc),
-        .row    (rows)
+        .enable (1'b1),
+        .out    (scan_out)
     );
     
+
+    // Assign row data
+    always_comb begin
+        case(scan_out)
+            3'b000: rows = 4'b1000; // Row 0 active
+            3'b001: rows = 4'b0100; // Row 1 active
+            3'b010: rows = 4'b0010; // Row 2 active
+            3'b011: rows = 4'b0001; // Row 3 active
+            default: rows = 4'b1111; // All rows inactive
+        endcase
+    end
+
     // Assigning final logic by reading from columns
     assign led[0] = cols[0];
     assign led[1] = cols[1];
--- fpga-keypad/src/lab2_scanner.sv
+++ fpga-keypad/src/lab2_scanner.sv
@@ -1,46 +1,28 @@
 // Stephen Kanti Mahanty - skantimahanty@hmc.edu
 // Made September 9, 2026
-// Multiplexer module that multiplexes two seven-segment displays
+// Used to be a counter to scan a keypad
 
-module lab2_multiplexer #(parameter width = 24, parameter logic [width-1:0] max_count = 2_999_999)(
-    input  logic reset, clk,
-    output logic [3:0] row);
-
-logic [3:0] state, nextstate;
-parameter S0 = 4'b1000, S1 = 4'b0100, S2 = 4'b0010, S3 = 4'b0001; // In order row[3], row[2], row[1], row[0]
-
-
+module counter #(parameter width = 24, parameter logic [width-1:0] max_count = 1_499_999)( // Params
+     input logic reset_n, clk, enable,
+     output logic [2:0] out
+ );
 
 logic [width-1:0] count = 0;
-   // Blink state
-   logic state = 0;
+
     // Counter
    always_ff @(posedge clk) begin
-     if(reset == 0) begin
+     if(reset_n == 0) begin
       count <= 0;
-      state <= S0;
      end
-     else begin
+     else if(enable) begin
       if(count == max_count) begin // Max count
-        state <= nextstate;
         count <= 0;
       end
       else count <= count + 1;
      end
    end
 
-// Next state logic
-always_comb begin
-    case(state)
-        S0: nextstate = S1;
-        S1: nextstate = S2;
-        S2: nextstate = S3;
-        S3: nextstate = S0;
-        default: nextstate = S0;
-    endcase
-end
-
-// Output Logic
-assign row[3:0] = state;
+   // Assign LED output
+   assign out = count % 4;
 
 endmodule
